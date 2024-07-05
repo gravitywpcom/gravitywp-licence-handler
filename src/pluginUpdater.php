fd5b3e02079add63b6e071e49e884e618a126874
Karim <kimoforworks@gmail.com>
2024-07-05 09:55:22 +0200

    fix for error Message php fatal error

--- src/pluginUpdater.php
+++ src/pluginUpdater.php
@@ -210,6 +210,9 @@ class Plugin_Updater {
 			} elseif ( isset( $json_data['message'] ) ) {
 				$this->error_messages = nl2br( $this->generateErrorMessage( $json_data['message'] ) );
 				return false;
+			} else {
+				$this->error_messages = nl2br( $this->generateErrorMessage( 'Please try again later. If the issue persists, please contact support.' ) );
+				return false;
 			}
 		}
 	}
